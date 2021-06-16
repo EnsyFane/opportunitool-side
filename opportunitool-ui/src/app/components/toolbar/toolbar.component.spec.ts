13c04fa62f639f4bb656770b6ba71a07babaf7df
Stefan Tataran <fane_ecl@yahoo.com>
2021-06-16 22:35:25 +0300

    Fixed tests

--- opportunitool-ui/src/app/components/toolbar/toolbar.component.spec.ts
+++ opportunitool-ui/src/app/components/toolbar/toolbar.component.spec.ts
@@ -1,4 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
 import { ToolbarComponent } from './toolbar.component';
 
@@ -8,9 +15,18 @@ describe('ToolbarComponent', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            declarations: [ToolbarComponent]
-        })
-            .compileComponents();
+            declarations: [ToolbarComponent],
+            imports: [
+                AppRoutingModule,
+                BrowserAnimationsModule,
+                FormsModule,
+                MatButtonModule,
+                MatIconModule,
+                MatInputModule,
+                MatToolbarModule,
+                ReactiveFormsModule
+            ]
+        }).compileComponents();
     });
 
     beforeEach(() => {
