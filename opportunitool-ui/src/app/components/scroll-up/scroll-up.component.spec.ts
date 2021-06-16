c57c408e4083da110caae909577b1f5e0d090f52
Stefan Tataran <fane_ecl@yahoo.com>
2021-06-17 02:03:08 +0300

    Fixed tests

--- opportunitool-ui/src/app/components/scroll-up/scroll-up.component.spec.ts
+++ opportunitool-ui/src/app/components/scroll-up/scroll-up.component.spec.ts
@@ -1,25 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatIconModule } from '@angular/material/icon';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { ScrollUpComponent } from './scroll-up.component';
 
 describe('ScrollUpComponent', () => {
-  let component: ScrollUpComponent;
-  let fixture: ComponentFixture<ScrollUpComponent>;
+    let component: ScrollUpComponent;
+    let fixture: ComponentFixture<ScrollUpComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ScrollUpComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [ScrollUpComponent],
+            imports: [
+                MatIconModule,
+                NoopAnimationsModule
+            ]
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ScrollUpComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ScrollUpComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
